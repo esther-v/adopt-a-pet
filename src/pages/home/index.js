5b8c171fd5d748328e0707ef2b2b15cc144d2ef9
esther-v <esvalas@gmail.com>
2022-03-22 17:09:00 +0100

    done routing

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState} from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
-//import Pet from '../../components/pet';
+import Pet from '../../components/pet';
 import { useParams, Link } from 'react-router-dom';
 
 const HomePage = () => {
